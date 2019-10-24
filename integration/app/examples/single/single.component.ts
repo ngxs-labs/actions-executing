6e35dc81f133a65910626a8908eb8e92be68f6e7
Joaquin Cid <joaqcid@gmail.com>
2019-10-24 08:33:04 -0300

    feat: include count

--- integration/app/examples/single/single.component.ts
+++ integration/app/examples/single/single.component.ts
@@ -1,24 +1,32 @@
 import { Observable } from 'rxjs';
 import { actionsExecuting, ActionsExecuting } from '@ngxs-labs/actions-executing';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { ZooState } from '../states/zoo/zoo.state';
 import { ZooStateModel } from '../states/zoo/zoo.model';
 import { AddBear, AddPanda } from '../states/zoo/zoo.actions';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'single',
     templateUrl: 'single.component.html'
 })
-export class SingleComponent {
+export class SingleComponent implements OnInit {
 
     @Select(actionsExecuting([AddPanda])) public addPandaExecuting$: Observable<ActionsExecuting>;
     @Select(actionsExecuting([AddBear])) public addBearExecuting$: Observable<ActionsExecuting>;
+    addBearExecutingCount$: Observable<number>;
     @Select(ZooState) public zoo$: Observable<ZooStateModel>;
 
     constructor(
         private store: Store
     ) { }
+
+    ngOnInit() {
+        this.addBearExecutingCount$ = this.addBearExecuting$.pipe(
+            map(actionsExecuting => actionsExecuting[AddBear.type])
+        )
+    }
 
 
     public addPanda() {
