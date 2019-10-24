57ca0e321f5377eb938232a680c1f83d45cc3ab1
Joaquin Cid <joaqcid@gmail.com>
2019-10-24 08:30:36 -0300

    styles: bootstrap

--- integration/app/app.component.ts
+++ integration/app/app.component.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
