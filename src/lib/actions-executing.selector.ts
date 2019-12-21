5bc15cddad38e061690335f78cebc49a4718681b
Joaquin Cid <joaqcid@gmail.com>
2019-12-21 11:14:33 -0300

    fix: types

--- src/lib/actions-executing.selector.ts
+++ src/lib/actions-executing.selector.ts
@@ -1,13 +1,13 @@
-import { createSelector, getActionTypeFromInstance } from '@ngxs/store';
+import { createSelector, getActionTypeFromInstance, ActionType } from '@ngxs/store';
 import { ActionsExecutingState, ActionsExecutingStateModel } from './actions-executing.state';
 
 export type ActionsExecuting = { [action: string]: number } | null;
 
-export function actionsExecuting(actionTypes: any[]) {
+export function actionsExecuting(actionTypes: ActionType[]) {
     return createSelector(
         [ActionsExecutingState],
         (state: ActionsExecutingStateModel): ActionsExecuting => {
-            return actionTypes.reduce((acc: any, type: any) => {
+            return actionTypes.reduce((acc: ActionsExecuting, type: ActionType) => {
                 const actionType = getActionTypeFromInstance(type);
 
                 if (!actionType) {
