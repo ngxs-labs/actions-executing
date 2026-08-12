e7dbe8bb0b679a1f45d1d96cbb2995de08cb3bc5
joaq cid <joaqcid@gmail.com>
2026-08-12 10:33:33 -0300

    chore: lint

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,6 @@ import type { Config } from 'jest';
 import { createCjsPreset } from 'jest-preset-angular/presets/index.js';
 
 export default {
-    ...createCjsPreset(),
-    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts']
+  ...createCjsPreset(),
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
 } satisfies Config;
