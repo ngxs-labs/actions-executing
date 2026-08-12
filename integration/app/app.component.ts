8615910d7581e0a21a8f906912581a8d3e0228d6
joaq cid <joaqcid@gmail.com>
2026-08-12 10:06:44 -0300

    chore: lint

--- integration/app/app.component.ts
+++ integration/app/app.component.ts
@@ -1,16 +1,15 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    changeDetection: ChangeDetectionStrategy.Eager,
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Eager,
+  standalone: false,
 })
 export class AppComponent {
-    public store$: Observable<unknown> = this.store.select((state) => state);
+  store = inject(Store);
 
-    constructor(private store: Store) {}
+  store$ = this.store.select((state: unknown) => state);
 }
